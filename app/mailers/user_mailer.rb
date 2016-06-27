019d6e7657c723ad1090ba5bab8d31b4d80227be
malikkamraniqbal <kamran@teespire.com>
2016-06-27 22:16:51 +0500

    configurations for email settings changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "Harry's <welcome@harrys.com>"
+  default from: ENV['GMAIL_ADDRESS']
 
   def signup_email(user)
     @user = user
-    @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+    @twitter_message = "Welcome to our test server! Have a nice day!!!"
 
-    mail(:to => user.email, :subject => "Thanks for signing up!")
+    mail(:to => user.email, :subject => "[Testing]Thanks for signing up!")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,15 @@ end
 Prelaunchr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+    ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "mail.google.com",
+    :user_name            => ENV['GMAIL_ADDRESS'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
